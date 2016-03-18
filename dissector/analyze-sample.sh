7d0b273a19ac0374fa933504715a3df1374770a0
SergioLazaro <sergiozgz1994@gmail.com>
2016-03-18 14:20:19 +0100

    Shell script added to iterate over a directory

--- dissector/analyze-sample.sh
+++ dissector/analyze-sample.sh
@@ -6,6 +6,7 @@ elif [ ! -d $1 ];then
     echo "Parameter should be a directory"
 else
     for elem in `ls $1`;do
-        python main.py -v "5.1.1" -i "$1" -o "/home/sid/android/malware/analysis"
+        echo "main.py -v 5.1.1 -i $1 -o /home/sid/android/malware/analysis"
+        #python main.py -v "5.1.1" -i "$1" -o "/home/sid/android/malware/analysis"
     done
 fi
