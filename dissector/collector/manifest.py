91f3bc90bd7de2c0f4850f9836f511ed32447c5d
vaioco <valerio.costamagna@unito.it>
2016-03-18 15:47:10 +0100

    dbs

--- dissector/collector/manifest.py
+++ dissector/collector/manifest.py
@@ -117,7 +117,7 @@ class Manifest(Acollector):
         #Creating directory if not exists
         if not os.path.exists(dir):
             print "Creating directory " + str(dir) + " for APK " + str(apkname) + "..."
-            os.mkdir(dir)
+            os.mkdirdirs(dir)
             os.chmod(dir,0755)
 
         db = PScoutDB(version)
