f13bfffec822145106ee5c74b4ffff10b466fa6e
vaioco <valerio.costamagna@unito.it>
2016-03-18 15:50:21 +0100

    dbs

--- dissector/analyze-sample.sh
+++ dissector/analyze-sample.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+PWD=`pwd`
 if [ -z $1 ];then
     echo "Invalid parameter"
 elif [ ! -d $1 ];then
     echo "Parameter should be a directory"
 else
     for elem in `ls $1`;do
-        echo "main.py -v 5.1.1 -i $1 -o /home/sid/android/malware/analysis"
+        if [ -f $elem ]; then
+            python main.py -v 5.1.1 -i "$PWD/$1" -o /tmp/asd
+        fi
         #python main.py -v "5.1.1" -i "$1" -o "/home/sid/android/malware/analysis"
     done
 fi
