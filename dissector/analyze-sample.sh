53ee682dd94522f142f25bbf15c6a21af705b156
SergioLazaro <sergiozgz1994@gmail.com>
2016-03-18 14:16:16 +0100

    Shell script added to iterate over a directory

---
+++ dissector/analyze-sample.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z $1 ];then
+    echo "Invalid parameter"
+elif [ ! -d $1 ];then
+    echo "Parameter should be a directory"
+else
+    for elem in `ls $1`;do
+        python main.py -v "5.1.1" -i "$1" -o "/home/sid/android/malware/analysis"
+    done
+fi
