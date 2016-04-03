7faff9aacab9cf929a564d2f7ce8827eebc5ace1
Sergio Lazaro <sergiozgz1994@gmail.com>
2016-04-03 19:10:31 +0200

    Added python script to download APKS from VirusTotal

--- dissector/download-apks.py
+++ dissector/download-apks.py
@@ -63,7 +63,7 @@ def download(key,apk):
     i = 0
     #while i < 10 or response.status_code is not 200:
     print "Trying " + str(i+1) + " attempt"
-    time.sleep(3000)
+    #time.sleep(3000)
     #    response = requests.get('https://www.virustotal.com/vtapi/v2/file/download', params=params)
     #    i += 1
     element = response.content
