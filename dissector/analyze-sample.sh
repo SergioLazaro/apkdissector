af179a54c7feb508b1ee46f9f5f53f021b69cf2d
SergioLazaro <sergiozgz1994@gmail.com>
2016-04-04 11:34:18 +0200

    Modified analyze-sample.sh

--- dissector/analyze-sample.sh
+++ dissector/analyze-sample.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 PWD=`pwd`
-echo $PWD
-echo $1
 if [ -z $1 ];then
     echo "Invalid parameter"
 elif [ ! -d $1 ];then
     echo "Parameter should be a directory"
 else
     for elem in `ls $1`;do
-        if [ -f $elem ]; then
-            python /home/sid/android/apkdissector/dissector/main.py -v 5.1.1 -i "$PWD/$1" -o /tmp/asd
+        if [ -f "$1/$elem" ]; then
+            python /home/sid/android/apkdissector/dissector/main.py -v 5.1.1 -i "$1/$elem" -o /tmp/asd
         fi
     done
 fi
