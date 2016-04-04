e2eb518361ed081c75027f5bf674634224c32f67
SergioLazaro <sergiozgz1994@gmail.com>
2016-04-04 11:32:04 +0200

    Modified analyze-sample.sh

--- dissector/analyze-sample.sh
+++ dissector/analyze-sample.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 PWD=`pwd`
+echo $PWD
+echo $1
 if [ -z $1 ];then
     echo "Invalid parameter"
 elif [ ! -d $1 ];then
@@ -10,6 +12,5 @@ else
         if [ -f $elem ]; then
             python /home/sid/android/apkdissector/dissector/main.py -v 5.1.1 -i "$PWD/$1" -o /tmp/asd
         fi
-        #python main.py -v "5.1.1" -i "$1" -o "/home/sid/android/malware/analysis"
     done
 fi
