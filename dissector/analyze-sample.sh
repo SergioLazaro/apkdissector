9d9438a13715ba3b82c418f46c36031a87b2a70a
SergioLazaro <sergiozgz1994@gmail.com>
2016-04-04 11:34:53 +0200

    Modified analyze-sample.sh

--- dissector/analyze-sample.sh
+++ dissector/analyze-sample.sh
@@ -7,8 +7,8 @@ elif [ ! -d $1 ];then
     echo "Parameter should be a directory"
 else
     for elem in `ls $1`;do
-        if [ -f "$1/$elem" ]; then
-            python /home/sid/android/apkdissector/dissector/main.py -v 5.1.1 -i "$1/$elem" -o /tmp/asd
+        if [ -f "$1$elem" ]; then
+            python /home/sid/android/apkdissector/dissector/main.py -v 5.1.1 -i "$1$elem" -o /tmp/asd
         fi
     done
 fi
