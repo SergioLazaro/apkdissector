9026df8a3a70610a3e805ee0a6beca7f8ebc10cb
SergioLazaro <sergiozgz1994@gmail.com>
2016-04-04 12:30:01 +0200

    Modified analyze-sample.sh

--- dissector/analyze-sample.sh
+++ dissector/analyze-sample.sh
@@ -8,7 +8,7 @@ elif [ ! -d $1 ];then
 else
     for elem in `ls $1`;do
         if [ -f "$1$elem" ]; then
-            python /home/sid/android/apkdissector/dissector/main.py -v 5.1.1 -i "$1$elem" -o /tmp/asd
+            python /home/sid/android/apkdissector/dissector/main.py -v 5.1.1 -i "$1$elem" -o /tmp/sd1
         fi
     done
 fi
