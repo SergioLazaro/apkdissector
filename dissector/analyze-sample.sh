6f72a5aa951b85800916a9ef576b88ca5ac905c0
SergioLazaro <sergiozgz1994@gmail.com>
2016-04-14 10:36:33 +0200

    Added multi-threading functionality

--- dissector/analyze-sample.sh
+++ dissector/analyze-sample.sh
@@ -7,14 +7,17 @@ elif [ ! -d $1 ];then
     echo "Parameter should be a directory"
 else
     i=0
+    output=""
     for elem in `ls $1`;do
         if [ -f "$1$elem" ]; then
             if [ $i -lt 5 ]; then
                 echo "Launching thread $i"
                 python /home/sid/android/apkdissector/dissector/main.py -v 5.1.1 -i "$1$elem" -o /tmp/clean-sd1 &
                 i=$(($i + 1))
+                output=$output"\n"$elem
             else
                 #Waiting for the threads launched
+                echo $output
                 wait
                 i=0
                 #Launch the element we have when i = 5
