c4d394e0b55395dea4c3d604d33b0416268d7619
SergioLazaro <sergiozgz1994@gmail.com>
2016-04-26 16:45:29 +0200

    Catching Androguard exceptions

--- dissector/core/threadAnalyzer.py
+++ dissector/core/threadAnalyzer.py
@@ -55,7 +55,7 @@ class ThreadAnalyzer (threading.Thread):
         except:
             print "Error appeared analyzing " + static_target
             fd = open(config.outputdir + "errors/" + apkname + ".txt","w")
-            err = traceback.extract_stack()
+            err = traceback.format_exc()
             fd.write("ERROR: " + str(err))
             fd.close
         #deob = Deobfuscator(targetapp)
