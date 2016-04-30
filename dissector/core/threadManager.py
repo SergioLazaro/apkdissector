5f5393787fd25a148b36df3f5a07f36aa26a938d
valerio <valerio.costamagna@di.unito.it>
2016-04-30 14:43:19 +0200

    ThreadManager

--- dissector/core/threadManager.py
+++ dissector/core/threadManager.py
@@ -25,37 +25,7 @@ class Worker(threading.Thread):
         threading.Thread.__init__(self)
         self.tasks = tasks
     def run(self):
-<<<<<<< HEAD
         task = self.tasks.get()
         task.start()
         print "Worker task finished!!"
         self.tasks.task_done()
-=======
-        print "Worker " + str(self.id) + " go to sleep " + str(self.t) + " seconds."
-        time.sleep(self.t)
-        print "Worker " + str(self.id) + " finished"
-        if self.lock.locked():
-            print "Thread " + str(self.id) + " RELEASE"
-            try:
-                self.lock.release()
-            except:
-                print "Lock unlocked."
-            self.working -= 1
->>>>>>> 020840b949167fbf6c50c2b8fcd58ffb618fa135
-'''
-start = time.time()
-tm = ThreadManager(2)
-for i in range(5):      #we launch 5 tasks but can be run just 3
-    print "WORKING : " + str(tm.working) + " LOCK STATE: " + str(tm.lock.locked())
-    waittime = random.randint(8,10)
-    t = Worker(i,waittime,tm.lock,tm.working)
-    tm.manage(t)
-    time.sleep(2)
-waiting = threading.enumerate()
-for thread in waiting[1:]:
-    thread.join()
-end = time.time()
-print "Total time %.2f" % (end - start)
-'''
-
-'''
