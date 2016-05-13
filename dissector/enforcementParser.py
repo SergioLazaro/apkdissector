b74cca71511b23fa6c113b09e31cc7bdf5ed305e
SergioLazaro <sergiozgz1994@gmail.com>
2016-05-13 16:34:19 +0200

    Added changes in parameters checker

--- dissector/enforcementParser.py
+++ dissector/enforcementParser.py
@@ -1,23 +1,61 @@
 __author__ = 'sergio'
 
 import optparse
-from enforcement.enforceReader import Reader
+from enforcement.enforceReader import Fixer
 from enforcement.permMapParser import AndroidJsonParser
 from enforcement.permStackDb import PermStackDb
 
-def main(jsonpath,permission,dbpath,output):
+'''
+    Method called when all the options are setted up.
+'''
+def main(jsonpath,permission,dbpath,jsondb):
 
-    reader = Reader(jsonpath,dbpath)
-    #reader.checkPermissionInfo(permission)
-    jsoninfo = AndroidJsonParser(jsonpath)
+    reader = Fixer(jsonpath)        #Fixing the json file if is not well formed
+
+    jsoninfo = AndroidJsonParser(jsonpath)  #Parsing the JSON file
     print "[*] Total elements read: " + str(len(jsoninfo.permissionStackElementList))
     print "[*] Creating SQLite database to store the information..."
-    permStackDb = PermStackDb(jsoninfo.permissionStackElementList,output)
+    permStackDB = PermStackDb(jsoninfo.permissionStackElementList,jsondb,permission)
+
+    #SHOULD CONTINUE WITH MORE OPTIONS CODE
+
     print "[*] Execution finished successfuly"
 
+'''
+    Method called when the options [-p] and [-j] are setted up
+'''
+def jsonDBpermissionCheck(permission,jsondb):
+    permStackDB = PermStackDb(None,jsondb,permission)
+
+'''
+    Method called when the options [-f], [-j] and [-p] are
+    setted up. This function is also called when [-f] and
+    [-j] are setted up, without [-p]
+'''
+def createjsonDBandCheckPermission(jsonpath,jsondb,permission):
+
+    print "[*] Checking if JSON file is well formed..."
+    reader = Fixer(jsonpath)        #Fixing the json file if its not well formed
+    print "[*] Reading JSON file..."
+    jsoninfo = AndroidJsonParser(jsonpath)  #Parsing the JSON file
+    print "[*] Total elements read: " + str(len(jsoninfo.permissionStackElementList))
+    print "[*] Creating SQLite database to store the information..."
+    permStackDB = PermStackDb(jsoninfo.permissionStackElementList,jsondb,permission)
+
+def checkPermissionBothDBs(jsondb,pscoutdb,permission):
+    print "TO DO"
+
 def print_help(parser):
-    print "arguments error!!\n"
     parser.print_help()
+    print "COMMAND HELP EXAMPLES:\n"
+    print "1) Parse JSON file and create a DB"
+    print "\tpython enforcementParser.py -f json/file/path.json -j /output/directory/\n"
+    print "2) Parse JSON file, create DB and check a permission"
+    print "\tpython enforcementParser.py -f json/file/path.json -j /output/directory/ -p android.permission.READ_PHONE_STATE\n"
+    print "3) Check a permission on a created database"
+    print "\tpython enforcementParser.py -j /path/json/database.db -p android.permission.READ_PHONE_STATE\n"
+    print "4) Check permission mapping with PScout DB and JSON DB"
+    print "\tpython enforcementParser.py -j /path/json/database.db -d /path/pscout/pscout.db -p android.permission.READ_PHONE_STATE\n"
     exit(-1)
 
 if __name__ == "__main__":
@@ -26,11 +64,25 @@ if __name__ == "__main__":
     parser.add_option('-f', '--file', action="store", help="Enforce permission JSON file path", dest="file",type="string"),
     parser.add_option('-p','--perm', action="store", help="Full permission name to analyze", dest="permission",type="string"),
     parser.add_option('-d', '--db', action="store", help="PScout DB path", dest="dbpath",type="string"),
-    parser.add_option('-o', '--output', action="store", help="SQLite DB generated with JSON file content ", dest="output",
-                      type="string")
+    parser.add_option('-j', '--jsondb', action="store", help="SQLite DB path to put it or path to a existing SQLite DB",
+                      dest="jsondb",type="string")
 
     (opts, args) = parser.parse_args()
-    if opts.file is not None and opts.permission is not None and opts.dbpath is not None and opts.output is not None:
-        main(opts.file,opts.permission,opts.dbpath,opts.output)
+    if opts.file is not None and opts.jsondb is not None:
+        if opts.permission is not None:
+            createjsonDBandCheckPermission(opts.file,opts.jsondb,opts.permission)
+        else:
+            createjsonDBandCheckPermission(opts.file,opts.jsondb,None)
+    elif opts.permission is not None and opts.jsondb is not None:
+        if opts.dbpath is not None:
+            #Compare jsonDB (self created) with the PScout DB
+            checkPermissionBothDBs(opts.jsondb,opts.dbpath,opts.permission)
+        else:
+            #Check db and print
+            jsonDBpermissionCheck(opts.permission, opts.jsondb)
+
+    elif opts.file is not None and opts.permission is not None and opts.dbpath is not None and opts.jsondb is not None:
+        main(opts.file,opts.permission,opts.dbpath,opts.jsondb)
+
     else:
         print_help(parser)
